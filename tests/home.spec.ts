ba15fd7c0306b836bd5db1fea4858087e5006704
P̴̻̅̉a̷͙͈̜͋̀t̷̟̓͠z̴̝̣̰͋͑ ̶̲̋̃̔ś̷͓̥̰L̸̖̗̼͑̒̀ <gm.teixeira@icloud.com>
2024-03-16 17:31:12 -0300

    fix: Corrected URL

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,15 +1,15 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:5173/', { waitUntil: 'networkidle' });
+  await page.goto('https://patzsl.github.io/hello-world/', { waitUntil: 'networkidle' });
   await expect(page).toHaveTitle(/Home/);
 });
 
 test('should go to about page', async ({ page }) => {
-  await page.goto('http://localhost:5173/', { timeout: 30000 }); // Aumenta o timeout para 30 segundos
+  await page.goto('https://patzsl.github.io/hello-world/', { timeout: 30000 }); // Aumenta o timeout para 30 segundos
  
   await page.locator('[data-test="btn-primary"]').click({ timeout: 10000 }); // Aumenta o timeout para 10 segundos
-  await expect(await page.url()).toBe('http://localhost:5173/about');
+  await expect(await page.url()).toBe('https://patzsl.github.io/hello-world/about');
   await page.pause();
  });
  
