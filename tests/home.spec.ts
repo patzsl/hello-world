effae3be832d8f325cae2e09816fca3395ecdbc3
P̴̻̅̉a̷͙͈̜͋̀t̷̟̓͠z̴̝̣̰͋͑ ̶̲̋̃̔ś̷͓̥̰L̸̖̗̼͑̒̀ <gm.teixeira@icloud.com>
2024-03-16 17:36:34 -0300

    fix: test url

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -9,7 +9,7 @@ test('should go to about page', async ({ page }) => {
   await page.goto('https://patzsl.github.io/hello-world/', { timeout: 30000 }); // Aumenta o timeout para 30 segundos
  
   await page.locator('[data-test="btn-primary"]').click({ timeout: 10000 }); // Aumenta o timeout para 10 segundos
-  await expect(await page.url()).toBe('https://patzsl.github.io/hello-world/about');
+  await expect(page).toHaveURL(/.*about/);
   await page.pause();
  });
  
